2b11f39aab73dae73c5196a61e0af69ce6a404c0
user <naveen-20678@naveen-20678.csez.zohocorpin.com>
2024-06-07 12:40:58 +0530

    updation

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,14 @@
-"use strict";
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+
 console.log("hello");
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
