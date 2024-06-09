f6bbd9998376f8e7d2a85f24e6007b85d8897460
naveennv2k <naveennv2k@gmail.com>
2024-06-09 13:05:21 +0530

    updation

--- dist/index.js
+++ dist/index.js
@@ -26206,7 +26206,7 @@ async function action() {
                 const secretPassword = json.secret.secretData[1].value;
                 core.exportVariable("secretUsername", secretUsername);
                 core.exportVariable("secretPassword", secretPassword);
-                core.setSecret("secretPassword");
+                core.setSecret(secretPassword);
                 // Log the cleaned output
                 // cleanOutput.forEach(line => console.log(line));
             });
