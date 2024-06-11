807e1c660a86091fd18b1fa995f5bf5b2f80bd2b
naveennv2k <naveennv2k@gmail.com>
2024-06-11 18:36:44 +0530

    updation

--- exec.sh
+++ exec.sh
@@ -6,11 +6,14 @@ set -e
 # Install op-cli
 install_op_cli() {
   # Create a temporary directory where the CLI is installed
-    
+  mkdir $GITHUB_WORKSPACE/zv
+  cd $GITHUB_WORKSPACE/zv
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
     curl  https://downloads.zohocdn.com/vault-cli-desktop/linux/zv_cli.zip --output zv_cli.zip
     unzip zv_cli.zip
     rm zv_cli.zip
+    ./zv login
     exit 0
 
     if [[ "$ARCH" != "386" ]] && [[ "$ARCH" != "amd64" ]] && [[ "$ARCH" != "arm" ]] && [[ "$ARCH" != "arm64" ]]; then
@@ -23,7 +26,7 @@ install_op_cli() {
     curl  https://downloads.zohocdn.com/vault-cli-desktop/macos/zv_cli.zip --output zv_cli.zip
     unzip zv_cli.zip
     rm zv_cli.zip
-    
+    ./zv login
   else
     echo "Operating system not supported yet for this GitHub Action: $OSTYPE."
     exit 1
--- installCli.js
+++
@@ -1,8 +0,0 @@
-const exec1=require("@actions/exec");
-console.log(process.platform);
-
-console.log("test1");
-  
-const cmd  =  exec1.getExecOutput(
-    "./exec.bat"
-);
